9908093f4427dc79c085a0f97607ad03ecd44e7b
timo-berg <timotheus.berg@pm.me>
2023-11-03 12:27:25 +0100

    Refactor Solve_Direct.jl to improve plotting and error analysis

--- Solve_Direct.jl
+++ Solve_Direct.jl
@@ -1,4 +1,3 @@
-using LinearSolve
 using Plots
 
 # Get the coefficient matrix A
@@ -53,14 +52,14 @@ f(x) = -2 * exp(x)
 # Correct solution
 u_exact(x) = exp(x) * (1 - x)
 
+mesh_sizes = [3, 6, 10]
+
 ### Plot exact solution
 x_exact = range(0, 1, length=1000)
-plot(x_exact, u_exact.(x_exact), label="Exact Solution")
-
-
+solution_plot = plot(x_exact, u_exact.(x_exact), label="Exact Solution", title="Exact Solution vs. Numerical Solution", xlabel="x", ylabel="u(x)")
 
 # Vary the mesh size
-for N in [3, 6, 10]
+for N in mesh_sizes
     # Get the coefficient matrix A and the right hand side vector b
     A = get_A(N, c)
     b = get_b(N, f, α, β)
@@ -77,8 +76,11 @@ end
 plot!(legend=:topright)
 
 
+mesh_sizes = 3:50
+
 ### Investigate the error
-for N in [3, 6, 10]
+errors = []
+for N in mesh_sizes
     # Get the coefficient matrix A and the right hand side vector b
     A = get_A(N, c)
     b = get_b(N, f, α, β)
@@ -89,7 +91,13 @@ for N in [3, 6, 10]
     # Plot the solution
     x_numeric = range(0, 1, length=N + 1)
 
-    errors = u - u_exact.(x_numeric[2:end-1])
-
-    println("N = $N, error = $(maximum(abs.(errors)))")
+    # Add error to the list
+    push!(errors, maximum(abs.(u - u_exact.(x_numeric[2:end-1]))))
 end
+
+# Plot the errors
+error_plot = scatter(mesh_sizes, errors, title="Discretization Error scaling with number of mesh points", xlabel="Mesh Points", ylabel="Error", legend=false)
+# plot!(mesh_sizes, 1 ./ mesh_sizes.^4)
+
+display(solution_plot)
+display(error_plot)
