ccad645d14c1e19e691644715a2d8c065821db27
hugosave <52716835+HugoSave@users.noreply.github.com>
2023-11-29 15:01:32 +0100

    Refactoring and adding gauss seidel analysis

---
+++ gauss_seidel.jl
@@ -0,0 +1,87 @@
+using LinearAlgebra
+using Plots
+using LaTeXStrings
+include("utils.jl")
+
+# Gets the Gauss Seidel error propagation matrix for a given A
+function get_BGS(A)
+    M = LowerTriangular(A)
+    B_GS = I - inv(M) * A
+    return B_GS
+end
+
+function plot_histogram_c_range()
+    N=100
+    p = plot()
+    c_values = [-5, -2,0, 2, 5] # [-5, -1, 0, 1, 5]
+    for c = c_values
+        A = get_A(N, c)
+        scatter!(p, eigvals(get_BGS(A)), label="C = $c", ms=3)
+    end
+    p
+end
+
+function plot_histogram_n_range()
+    c_value = 2
+    p = plot()
+    title!(p, L"Eigenvalues of $B_{GS}$ with $c = %$c_value$")
+    n_values = [10, 100, 200, 400] # [-5, -1, 0, 1, 5]
+    for n = n_values
+        A = get_A(n, c_value)
+        scatter!(p, eigvals(get_BGS(A)), label="N = $n", ms=3)
+    end
+    p
+end
+
+function plot_aboslute_values_c_range()
+    n_value = 100
+    c_values = [-5,  0, 5, 10, 100]
+    p = plot()
+    for c = c_values
+        A = get_A(n_value, c)
+        eigenvalues = eigvals(get_BGS(A))
+        abs_eig = map(x -> abs(x), eigenvalues)
+        scatter!(p, sort(abs_eig, rev=true), label=L"c=%$c", ms=3)
+    end
+    savefig(p, "plots/plot.pdf")
+    p
+end
+
+function plot_aboslute_values_n_range()
+    c_value = 10
+    n_values = [10, 100, 200, 400]
+    p = plot()
+    for n = n_values
+        A = get_A(n, c_value)
+        eigenvalues = eigvals(get_BGS(A))
+        abs_eig = map(x -> abs(x), eigenvalues)
+        scatter!(p, sort(abs_eig, rev=true), label=L"N=%$n", ms=3)
+    end
+    savefig(p, "plots/plot.pdf")
+    p
+end
+
+function test_get_BGS()
+    A = [2 -1 0; -1 2 -1; 0 -1 2]
+    B_expected = [0 1/2 0; 0 1/4 1/2; 0 1/8 1/4]
+    print(get_BGS(A),  B_expected)
+end
+
+# Test f at some points
+function test_construct_F()
+    f = construct_F(-1)
+    expected_f(x) = -2*exp(x)
+
+    print(f(42) == expected_f(42))
+
+    f_2 = construct_F(3)
+    expected_f_2(x) = 2*exp(x) - 4*x*exp(x)
+
+    print(f_2(42) == expected_f_2(42))
+
+end
+
+plot_aboslute_values_n_range()
+
+
+# plot_histogram_n_range()
---
+++ utils.jl
@@ -0,0 +1,46 @@
+# Get the coefficient matrix A for the Poisson equation
+function get_A(N, c)
+    h = 1 / N
+    A = zeros(N - 1, N - 1)
+
+    # Set the boundary points
+    A[1, 1] = c - 2 / h^2
+    A[1, 2] = 1 / h^2
+
+    A[N-1, N-2] = 1 / h^2
+    A[N-1, N-1] = c - 2 / h^2
+
+
+
+    # Set the interior points
+    for i = 2:N-2
+        A[i, i-1] = 1 / h^2
+        A[i, i] = c - 2 / h^2
+        A[i, i+1] = 1 / h^2
+    end
+
+    return A
+end
+
+# Get the right hand side vector b
+function get_b(N, f, α=1, β=0)
+    h = 1 / N
+
+    b = zeros(N - 1)
+
+    # Set the boundary points
+    b[1] = f(h) - α / h^2
+    b[N-1] = f(1 - h) - β / h^2
+
+    # Set the interior points
+    for i = 2:N-2
+        b[i] = f(i * h)
+    end
+
+    return b
+end
+
+# Returns the f function given a certain c
+function construct_F(c)
+    return function f(x) exp(x) * (c-1) - x*exp(x) *(1+c) end
+end
