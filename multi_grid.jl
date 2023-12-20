da24472661809ac842e8084c485c8b1ced1df78b
timo-berg <timotheus.berg@pm.me>
2023-12-20 13:41:17 +0100

    Fixed wrong implementation of the mapping functions

--- multi_grid.jl
+++ multi_grid.jl
@@ -1,43 +1,33 @@
 include("utils.jl")
+using LinearAlgebra
 
+function linear_interpolation(h, H)
+    I = zeros(h, H)
 
-function linear_interpolation(H, h)
-    I = zeros(H, h)
-    # First column 
-    I[1, 1] = 2
-    I[2, 1] = 1
-
-
-    # Interior points
-    for i = 2:h
-        I[2*i-2, i] = 1
-        I[2*i-1, i] = 2
-        I[2*i, i] = 1
+    for i = 1:H
+        I[2*i-1, i] = 1
+        I[2*i, i] = 2
+        I[2*i+1, i] = 1
     end
 
     return I * 1 / 2
 end
 
-function half_weight_mapping(H, h)
-    I = zeros(h, H)
-    # First row 
-    I[1, 1] = 2
-    I[1, 2] = 1
-
-
+function half_weight_mapping(h, H)
+    I = zeros(H, h)
     # Interior points
-    for i = 2:h
-        I[i, 2*i-2] = 1
-        I[i, 2*i-1] = 2
-        I[i, 2*i] = 1
+    for i = 1:H
+        I[i, 2*i-1] = 1
+        I[i, 2*i] = 2
+        I[i, 2*i+1] = 1
     end
 
     return I * 1 / 4
 end
 
 function get_B_CGC(H, h, c)
-    I_h_to_H = linear_interpolation(H, h)
-    I_H_to_h = half_weight_mapping(H, h)
+    I_H_to_h = linear_interpolation(h, H)
+    I_h_to_H = half_weight_mapping(h, H)
     A = get_A(h + 1, c)
 
     A_H_inv = inv(I_h_to_H * A * I_H_to_h)
