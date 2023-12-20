cc8ac10094b3b581220e9ef15d858fa97c5ca454
timo-berg <timotheus.berg@pm.me>
2023-12-20 13:53:43 +0100

    Working solution for task 8

--- task_8.jl
+++ task_8.jl
@@ -1,5 +1,29 @@
 using LinearAlgebra
 using Plots
+using LaTeXStrings
 
-# TODO: gives error because inverse of A_H is singular. Probably error in multi_grid.jl
+###### Works but not sure if its correct? 
+
+N = 300
+c = 60
+h = N - 1
+H = Int(N / 2 - 1)
+
+
 B_CGC = get_B_CGC(H, h, c)
+
+B_eigvals = eigvals(B_CGC)
+
+# Plot the eigenvalues with real part < 0.5
+p = plot(title=L"Small Eigenvalues of $B_{CGC}$")
+small_eigvals = [eigval for eigval in B_eigvals if real(eigval) < 0.5]
+scatter!(p, real.(small_eigvals), imag.(small_eigvals), label="", ms=3)
+
+# Plot the eigenvalues with real part > 0.5
+q = plot(title=L"Large Eigenvalues of $B_{CGC}$")
+large_eigvals = [eigval for eigval in B_eigvals if real(eigval) > 0.5]
+scatter!(q, real.(large_eigvals), imag.(large_eigvals), label="", ms=3)
+
+
+# Combine the plots
+plot(p, q, layout=(1, 2), size=(1000, 500))
