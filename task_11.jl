ce9faaec48f9b3f589e2a850eb14a33b57b473e9
hugosave <52716835+HugoSave@users.noreply.github.com>
2024-01-11 19:55:27 +0100

    Added effective condition number

--- task_11.jl
+++ task_11.jl
@@ -1,23 +1,21 @@
 using LinearAlgebra
 using Plots
 using LaTeXStrings
+using Printf
 include("multi_grid.jl")
 include("gauss_seidel.jl")
 include("conjugate_gradient.jl")
 include("plot_def.jl")
 
-function get_projection_matrix(N, A)
+function get_projection_matrix(A)
     M_inv = get_M_CGC_inv(A)
     return I - A * M_inv
 end
 
-function solve_projected_CG(A, b, N)
-    P = get_projection_matrix(N, A)
-    A_projected = P * A
-    b_projected = P * b
-    init_guess = zeros(N - 1)
-    u, residuals, iters_used = conjugate_gradient(A_projected, b_projected, init_guess, 1e-6, 1000)
-    return u, residuals, iters_used
+function get_effective_condition_number(Ah)
+    eigenvalues = eigvals(Ah)
+    min_nonzero_eigenvalue = minimum(eigenvalues[eigenvalues .> 1e-8])
+    return maximum(eigenvalues) / min_nonzero_eigenvalue 
 end
 
 function task11_residual_plot()
@@ -25,13 +23,17 @@ function task11_residual_plot()
     N_values = [10, 100, 300]
     c_values = [50, 100]
     p = plot(xlabel="Iteration", ylabel="Log Residual Error", yscale=:log10, title="Convergence of Projected CG")
-    yticks!(10.0 .^ (-5:1:4))
+    xticks!(p, 1:13)
     for N in N_values
         for c in c_values
             A = get_A(N, c)
             b = get_b(N, construct_F(c))
-            _, errors, iters_used = solve_projected_CG(A, b, N)
-            plot!(0:iters_used, errors, label="N = $N, c = $c")
+            P = get_projection_matrix(A)
+            A_projected = Symmetric(P * A) # From analytical inspection this matrix is symmetric
+            b_projected = P * b
+            _, errors, iters_used, _ = conjugate_gradient(A_projected, b_projected, zeros(N-1), 1e-9, 2000)
+            eff_cond = get_effective_condition_number(A_projected)
+            plot!(errors, label="N = $N, c = $c, kappa = $(@sprintf("%.4f", eff_cond))")
         end
     end
     p
@@ -39,4 +41,4 @@ end
 
 task11_residual_plot()
 
-savefig("plots/task_11_residual.png")
+# savefig("plots/task_11_residual.png")
